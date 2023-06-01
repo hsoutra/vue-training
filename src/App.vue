394d6915a6e4dcffc601bd2463646ff4ccea31c7
Hachim Soutra <hsoutra@smb.sqli.com>
2023-06-01 08:41:45 +0100

    initila commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+ const showModal = ref(false)
+</script>
+
+<template>
+  <header>
+    <h1>
+      Notes
+    </h1>
+    <button @click="showModal = true">+</button>
+  </header>
+  <main>
+    {{ showModal ? "show modal" : "hide Modal" }}
+  </main>
+</template>
