3a5df95fe73c757e51745ce46a9160ef2cb4b907
Hachim Soutra <hsoutra@smb.sqli.com>
2023-06-20 10:07:08 +0100

    clean code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
 import { ref } from "vue";
 
- const showModal = ref(false)
 </script>
 
 <template>
-  <header>
-    <h1>
-      Notes
-    </h1>
-    <button @click="showModal = true">+</button>
-  </header>
   <main>
-    {{ showModal ? "show modal" : "hide Modal" }}
+    hello world
   </main>
 </template>
