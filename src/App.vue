9c78f0e8a604dab7440ac0d72e896956262ec287
Hachim Soutra <hsoutra@smb.sqli.com>
2023-06-20 11:27:54 +0100

    add store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup>
-import { ref } from "vue";
+import { useCounterStore } from '@/stores/counter'
 
+const counter = useCounterStore()
+
+function logJSONData() {
+  console.time('timer')
+  fetch("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    console.timeEnd('timer')
+    counter.load(res);
+    console.timeEnd('timer')
+  })
+  
+}
 </script>
 
 <template>
   <main>
     hello world
+    <br>
+    <button @click="logJSONData">fetch data</button>
+    <br>
+    <div>
+      {{ counter.data }}
+    </div>
   </main>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+
+createApp(App)
+.use(pinia)
+.mount('#app')
